67836651983d29a769c28f5a1ae66b9f84ac867c
Guil H <g.hernandez07@gmail.com>
2022-06-23 12:39:05 -0400

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
+import { Routes, Route } from "react-router-dom";
 import logo from "../assets/red30-tech-logo.png";
+
+import Home from "./Home";
+import Categories from "./Categories";
 
 function App() {
   return (
@@ -11,6 +15,11 @@ function App() {
           title="Red30 Tech | Home"
         />
       </header>
+
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
 
       <footer className="container">
         &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
